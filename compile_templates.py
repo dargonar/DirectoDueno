5e4f7ba86b836efe4f86720e0fae5669284035b9
Pablo <ptutino@gmail.com>
2012-01-17 17:17:01 -0300

    Version 1.0.15

--- compile_templates.py
+++ compile_templates.py
@@ -152,7 +152,7 @@ def compile_templates(argv=None):
     FileSystemLoader.list_templates = list_templates
 
     env = jinja2.get_jinja2(app=app).environment
-    from myfilters import do_currencyfy, do_statusfy, do_pricefy, do_addressify, do_descriptify, do_headlinify, do_slugify, do_operationfy, do_totalareafy, do_expensasfy, do_add_days,  do_ownerify, do_oper_labelify
+    from myfilters import do_currencyfy, do_statusfy, do_pricefy, do_addressify, do_descriptify, do_headlinify, do_slugify, do_operationfy, do_totalareafy, do_expensasfy, do_add_days,  do_ownerify, do_oper_labelify, do_email_to_png
 
     env.filters['currencyfy']         = do_currencyfy
     env.filters['statusfy']           = do_statusfy
@@ -167,6 +167,7 @@ def compile_templates(argv=None):
     env.filters['add_days']           = do_add_days
     env.filters['ownerify']           = do_ownerify
     env.filters['oper_labelify']      = do_oper_labelify
+    env.filters['email_to_png']      = do_email_to_png
     
     # from myfilters import do_currencyfy, do_statusfy, do_pricefy, do_addressify, do_descriptify, do_headlinify, do_slugify, do_operationfy, do_totalareafy, do_expensasfy, do_add_days, do_ownerify
 
